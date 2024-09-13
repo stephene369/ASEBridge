185f59a734319919c11dda5a931b9429f36955fb
stephene369 <stephenew36@gmail.com>
2024-09-13 14:32:11 +0200

    Version-2024/09/13-14:32:11

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from "@/components/ui/button"
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+        <div>
+      <Button>Click me</Button>
+    </div>    
       <h1 className="text-3xl font-bold underline">
     Hello world!
   </h1>
