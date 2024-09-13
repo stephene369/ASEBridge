7a7a3bca9b5a3ae2bf8b86382cd363cad1dc33d4
stephene369 <stephenew36@gmail.com>
2024-09-13 14:50:19 +0200

    Version-2024/09/13-14:50:19

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/asebridge/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "/asebridge/",
 })
